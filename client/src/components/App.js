db6cc37ae427301290607d21a8ffb9a75e51e29c
Alex Kasirer-Smibert <aksbass@gmail.com>
2022-08-19 17:20:28 -0400

    product page, itemCard and itemPage working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import GlobalStyles from "./GlobalStyles";
 import styled from 'styled-components';
 import ItemCard from './ItemCard';
+import ProductPage from './ProductPage';
 
 import Header from "./Header";
+import ItemPage from './ItemPage';
 
 const App = () => {
+  const [loading, setLoading] = useState(false)
   return (
     <BrowserRouter>
       {/* <GlobalStyles /> */}
@@ -16,7 +19,8 @@ const App = () => {
           <Route
             path="/"
           />
-          <Route path="/products/:_id" element={<ItemCard />}/>
+          <Route path="products" element={<ProductPage setLoading={setLoading} loading ={loading}/>}/>
+          <Route path="/products/:_id" element={<ItemPage/>}/>
           <Route
             path="/cart"
           />
--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,49 +1,26 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 
-const ItemCard = () => {
-  //state that will store the product object
-  const [product, setProduct] = useState(null);
-  //loading state that will change to true when the data is fetched from the backend, can add loading image while req is pending
-  const [loading, setLoading] = useState(false);
-  //used to navigate away from this page to the /cart page
-  let navigate = useNavigate();
-  //pulls the id from the URL to find the appropriate item
-  let  { _id } = useParams();
-  //fetch that will call the .get function in the backend to retrieve the getSingleItem handler
-  useEffect(() =>{
-    fetch(`/products/${_id}`)
-    .then(res => res.json())
-    .then((data) =>{
-      setProduct(data.data);
-      
-      setLoading(true); //loading state is now true, product state is updated with a single item
-      //navigate will be nested inside a button
-      // navigate('/cart', {state: { _id: _id}})
-    })
-  }, [])
-
-  const handleClick = () =>{
-    navigate('/cart', { state: { _id: _id}})
-  }
-  
+const ItemCard = (props) => {
+  const product = props.product
+  console.log(props)
   return (
     <Wrapper>
     {/* Conditional rendering below */}
-    {loading &&
+    {(props.loading) &&
       <>
-      <h1>{product.name}</h1>
+      <NavigationLink to={`/products/${product._id}`}>
       <img alt="base64 encoded URL of product" src={product.imageSrc}/>
       <div>
+        <p>{product.name}</p>
         <p>{product.price}</p>
-        <p>Quantity remaining: <span>{product.numInStock}</span></p>
       </div>
-      <Button onClick={handleClick}>Add to cart</Button>
-      </>
+      </NavigationLink>
+      
+      </>}
 
-    }
     </Wrapper>
   )
 }
@@ -51,8 +28,19 @@ const ItemCard = () => {
 
 export default ItemCard;
 
+const NavigationLink = styled(NavLink)`
+text-decoration: none;
+color:black;
+display:flex;
+flex-direction: column;
+align-items: center;
+`
+
 const Wrapper = styled.div`
 //CSS styling to be added
+width:200px;
+border:1px lightgray solid;
+padding: 5px;
 
 //bolding the quantity of the stock. can be removed 
 span {
