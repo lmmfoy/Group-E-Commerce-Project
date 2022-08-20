e067cdfa0b5c2ebc60a9bcfeecce4906a68e356b
Lea Foy <leafoy@gmail.com>
2022-08-20 13:39:46 -0400

    Update cart to simplify and use sessionStorage

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -3,62 +3,44 @@ import styled from "styled-components";
 
 const Cart = () => {
   // const location = useLocation().state;
-  const [cartItems, setCartItems] = useState([]);
-  const [cartTotal, setCartTotal] = useState(0);
+  const [cartState, setCartState] = useState(null);
+  let cart;
+
+  if (sessionStorage.getItem("cart")) {
+    cart = JSON.parse(sessionStorage.getItem("cart"));  
+  } else {
+    cart = {};
+  }
+
 
   useEffect(() => {
-    fetch(`/products/${6548}`)
+    fetch(`products/${6544}`)
       .then((res) => res.json())
       .then((data) => {
-        const { _id, name, price } = data.data;
+        const item = data.data;
+        
+        const itemID = item._id;
 
-        // Check to see if there are already items in the cartItems array
-        if (cartItems.length !== 0) {
-          let previouslyAddedItem = false;
-          // Map through the array, and if there is an item already in it that has the id of the item being added, increase the count
-          const newCartItemsArray = cartItems.map((item) => {
-            if (item._id === _id) {
-              previouslyAddedItem = true;
-              const newCount = item.count + 1;
-              const newItem = { ...item, count: newCount };
-              return newItem;
-            }
-            return item;
-          });
-
-          // If the item has never been added before, add to cartItems
-          if (previouslyAddedItem === false) {
-            setCartItems((prev) => [
-              ...prev,
-              { _id: _id, name: name, price: price, count: 1 },
-            ]);
-          } else {
-            // Update the cartItems array to the new array created by cartItems.map
-            setCartItems(newCartItemsArray);
-          }
+        if (!cart[itemID]) {
+          cart[itemID] = {...item, quantity: 1};
         } else {
-          // Add the first item to the cartItems array
-          setCartItems([{ _id: _id, name: name, price: price, count: 1 }]);
+          cart[itemID].quantity = cart[itemID].quantity + 1;
         }
-      })
-      .then(sessionStorage.setItem("cart", cartItems))
-  }, []);
-
-  console.log(cartItems);
-
+        sessionStorage.setItem("cart", JSON.stringify(cart))
+      });
+  });
 
   return (
     <StyledCart>
       <>
-        {cartItems.map((item) => {
+        {Object.values(cart).map((item) => {
           // Get just the price, without the dollar sign
-          console.log(item);
           const itemPrice = item.price.slice(1);
           return (
             <div class="cart-item">
               <span>{item.name}</span>
-              <span>{Math.round((itemPrice * item.count) * 100)/100}</span> 
-              <span>{item.count}</span>
+              <span>{Math.round(itemPrice * item.quantity * 100) / 100}</span>
+              <span>{item.quantity}</span>
             </div>
           );
         })}
