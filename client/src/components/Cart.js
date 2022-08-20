0c6412cecd8a8df6beeff2504cce3daeda877665
Lea Foy <leafoy@gmail.com>
2022-08-20 17:24:16 -0400

    Add number inputs to increase/decrease number of items

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,9 +1,10 @@
-import { useEffect, useState, useLocation } from "react";
+import { useEffect, useState, useLocation, useRef } from "react";
 import styled from "styled-components";
 
 const Cart = () => {
   // const location = useLocation();
   const [cartState, setCartState] = useState(null);
+
   let cart;
   let total = 0;
 
@@ -14,12 +15,15 @@ const Cart = () => {
     cart = {};
   }
 
-  // Calculate total 
+  // Calculate total
   Object.values(cart).map((item) => {
     const itemPrice = item.price.slice(1);
     const subTotal = (itemPrice * item.quantity).toFixed(2);
     total += parseFloat(subTotal);
   });
+
+  // This is to get the page to rerender when someone increases or decreases the number of items they want - probably something should actually happen in it
+  useEffect(() => {}, [cartState]);
 
   return (
     <StyledCart>
@@ -31,17 +35,32 @@ const Cart = () => {
             <div class="cart-item">
               <span>{item.name}</span>
               <span>{(itemPrice * item.quantity).toFixed(2)}</span>
-              <span>{item.quantity}</span>
+              <span>
+                <input
+                  id={item._id}
+                  type="number"
+                  value={item.quantity}
+                  min="0"
+                  max={item.numInStock}
+                  // When number changed, cart updated in sessionStorage and added to cartState to prompt useEffect to rerender page
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    cart[item._id] = { ...item, quantity: value };
+                    sessionStorage.setItem("cart", JSON.stringify(cart));
+                    setCartState(cart);
+                  }}
+                />{" "}
+              </span>
             </div>
           );
         })}
       </div>
-      <div>{total}</div>
+      <div>{total.toFixed(2)}</div>
     </StyledCart>
   );
 };
 
-const StyledCart = styled.div`
+const StyledCart = styled.form`
   .cart-item {
     padding: 20px;
 
