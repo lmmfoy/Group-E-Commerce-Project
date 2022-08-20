51b5804948effe43b0794c2d47614f66181ef1a1
Lea Foy <leafoy@gmail.com>
2022-08-20 16:03:06 -0400

    Add total price

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -5,6 +5,7 @@ const Cart = () => {
   // const location = useLocation();
   const [cartState, setCartState] = useState(null);
   let cart;
+  let total = 0;
 
   // Checks sessionStorage to see if there are already items in the cart, if not assigns cart variable empty object
   if (sessionStorage.getItem("cart")) {
@@ -13,38 +14,29 @@ const Cart = () => {
     cart = {};
   }
 
-  // useEffect(() => {
-  //   fetch(`products/${6547}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       const item = data.data;
-
-  //       const itemID = item._id;
-
-  //       if (!cart[itemID]) {
-  //         cart[itemID] = {...item, quantity: 1};
-  //       } else {
-  //         cart[itemID].quantity = cart[itemID].quantity + 1;
-  //       }
-  //       sessionStorage.setItem("cart", JSON.stringify(cart))
-  //     });
-  // });
+  // Calculate total 
+  Object.values(cart).map((item) => {
+    const itemPrice = item.price.slice(1);
+    const subTotal = (itemPrice * item.quantity).toFixed(2);
+    total += parseFloat(subTotal);
+  });
 
   return (
     <StyledCart>
-      <>
+      <div>
         {Object.values(cart).map((item) => {
           // Get just the price, without the dollar sign
           const itemPrice = item.price.slice(1);
           return (
             <div class="cart-item">
               <span>{item.name}</span>
-              <span>{Math.round(itemPrice * item.quantity * 100) / 100}</span>
+              <span>{(itemPrice * item.quantity).toFixed(2)}</span>
               <span>{item.quantity}</span>
             </div>
           );
         })}
-      </>
+      </div>
+      <div>{total}</div>
     </StyledCart>
   );
 };
