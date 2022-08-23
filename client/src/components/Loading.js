66bf261e35a7b4f3046201d6e7d3a5b976fbdaa3
Lea Foy <leafoy@gmail.com>
2022-08-23 11:01:51 -0400

    Add loading component

---
+++ client/src/components/Loading.js
@@ -0,0 +1,11 @@
+import { FallingLines } from "react-loader-spinner";
+
+const Loading = () => {
+    return <FallingLines
+        height="200"
+        width="200"
+        color="grey"
+    />
+}
+
+export default Loading;
