21255219a73cd8ef92f5068de1e1a9e20705f94f
Lea Foy <leafoy@gmail.com>
2022-08-23 12:57:58 -0400

    Add loading animation to Product page, CSS tweaks

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,11 +1,20 @@
 import { FallingLines } from "react-loader-spinner";
+import styled from "styled-components";
 
 const Loading = () => {
-    return <FallingLines
-        height="200"
-        width="200"
-        color="grey"
-    />
-}
+  return (
+    <StyledDiv>
+      <FallingLines height="200" width="200" color="grey" />
+    </StyledDiv>
+  );
+};
+
+const StyledDiv = styled.div`
+  margin: 150px 0 0 0;
+  padding: 0;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
 
 export default Loading;
