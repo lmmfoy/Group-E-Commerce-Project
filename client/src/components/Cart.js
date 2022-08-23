564ebeffc2335201dd0e33f943312acc226bb467
“Anna <annaovcharova84@gmail.com>
2022-08-23 16:28:55 -0400

    item-css-btn-done

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -222,29 +222,57 @@ const StyledCart = styled.form`
     padding-right: 35px;
 
     input {
-      font-size: 1.4em;
-      background-color: #dcdcdc;
-      padding: 10px;
-      border-radius: 5px;
+      padding: 12px 40px;
+      margin: 15px;
+      font: 16px/24px Verdana, sans-serif;
+      text-decoration: none;
+      background: 
+        linear-gradient(90deg, 
+          #04f5ed, 
+          #5c27fe, 
+          #c165dd, 
+          #04f5ed);
+      background-size: 400%;
+      display: inline-block;
+      color: #fff;
+      border-radius: 8px;
+      position: relative;
       border: none;
+      cursor: pointer;
+      animation: glowing 8s linear infinite;
+      text-transform: uppercase;
+      text-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+      letter-spacing: 1px;
+      &:after {
+        content: " ";
+        position: absolute;
+        border-radius: inherit;
+        left: -6px;
+        right: -6px;
+        bottom: -6px;
+        top: -6px;
+        background: inherit;
+        background-size: inherit;
+        z-index: -1;
+        opacity: 0;
+        transition: opacity 0.5s ease;
+        filter: blur(16px);
+        animation: glowing 7s linear infinite;
+        transform: translateZ(0);
+      }
+      &:hover {
+        &:after {
+          opacity: 1;
+        }
+      }
     }
-
-    input:hover {
-      background-color: #0099cc;
-      color: white;
-    }
-
-    input:active {
-      transform: scale(0.9);
+  
+  @keyframes glowing {
+    100% {
+      background-position: -400%;
     }
   }
-
-  .btn-remove {
-    
-    &:hover {
-
-    }
-  }
+}
 `;
 
 export default Cart;
