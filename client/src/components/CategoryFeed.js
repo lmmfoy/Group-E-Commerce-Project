a4b193a5d60ddd40aabdd3c6f62d69084f9f945d
“Anna <annaovcharova84@gmail.com>
2022-08-23 17:41:47 -0400

    confirmation-css-done

--- client/src/components/CategoryFeed.js
+++ client/src/components/CategoryFeed.js
@@ -6,102 +6,102 @@ import ItemCard from "./ItemCard";
 import Loading from "./Loading";
 
 const CategoryFeed = (props) => {
-  const { _category } = useParams();
-  const [items, setItems] = useState(null);
-  //loading state for fetch
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    fetch(`/products/categories/${_category}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(data.data);
-        props.setLoading(true);
-      })
-      .catch((err) => {
-        console.log("error");
-      });
-  }, []);
-  return (
-    <>
-      <ImageStyle>
-        <img src={img26} width="1000px" alt="watch" />
-        <div className="ImageText">
-          <h1>{_category}</h1>
-        </div>
-        <CategoryFeedBox>
-          {items ? (
-            items.map((item) => {
-              return (
-                <>
-                  <ItemProfile>
-                    <ItemCard product={item} loading={props.loading} />
-                  </ItemProfile>
-                </>
-              );
-            })
-          ) : (
-            // If still loading, show loading animation
-            <Loading />
-          )}
-        </CategoryFeedBox>
-      </ImageStyle>
-    </>
-  );
+    const { _category } = useParams();
+    const [items, setItems] = useState(null);
+    //loading state for fetch
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        fetch(`/products/categories/${_category}`)
+        .then((res) => res.json())
+        .then((data) => {
+            setItems(data.data);
+            props.setLoading(true);
+        })
+        .catch((err) => {
+            console.log("error");
+        });
+    }, []);
+    return (
+        <>
+        <ImageStyle>
+            <img src={img26} width="1000px" alt="watch" />
+            <div className="ImageText">
+            <h1>{_category}</h1>
+            </div>
+            <CategoryFeedBox>
+            {items ? (
+                items.map((item) => {
+                return (
+                    <>
+                    <ItemProfile>
+                        <ItemCard product={item} loading={props.loading} />
+                    </ItemProfile>
+                    </>
+                );
+                })
+            ) : (
+                // If still loading, show loading animation
+                <Loading />
+            )}
+            </CategoryFeedBox>
+        </ImageStyle>
+        </>
+    );
 };
 
 const ImageStyle = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-
-  img {
     width: 100%;
-    height: 50%;
-    object-fit: cover;
-  }
-
-  .ImageText {
-    position: absolute;
-    width: 100%;
-    height: 50%;
-    top: 64px;
+    height: 100vh;
     display: flex;
-    flex-direction: column;
     justify-content: center;
-    align-items: center;
-    color: white;
-    h1 {
-      font-size: 80px;
+
+    img {
+        width: 100%;
+        height: 50%;
+        object-fit: cover;
     }
-  }
-`;
+
+    .ImageText {
+        position: absolute;
+        width: 100%;
+        height: 50%;
+        top: 64px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        h1 {
+        font-size: 80px;
+        }
+    }
+    `;
 
 const CategoryFeedBox = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  position: absolute;
-  top: 60%;
-  max-width: 1400px;
-  display: flex;
-  justify-content: center;
-`;
+    display: flex;
+    flex-wrap: wrap;
+    position: absolute;
+    top: 60%;
+    max-width: 1400px;
+    display: flex;
+    justify-content: center;
+    `;
 
-const ItemProfile = styled.div`
-  display: flex;
-  background: white;
-  box-shadow: 10px 10px 8px 0 rgba(128, 128, 128, 0.44);
-  gap: 20px;
-  border-radius: 20px;
-  margin: 30px;
-  border: none;
-  font-size: 20px;
-  image-resolution: unset;
+    const ItemProfile = styled.div`
+    display: flex;
+    background: white;
+    box-shadow: 10px 10px 8px 0 rgba(128, 128, 128, 0.44);
+    gap: 20px;
+    border-radius: 20px;
+    margin: 30px;
+    border: none;
+    font-size: 20px;
+    image-resolution: unset;
 
-  :hover {
-    box-shadow: 0 0 15px 1px #5bb318;
-    opacity: 70%;
-  }
+    :hover {
+        box-shadow: 0 0 15px 1px #5bb318;
+        opacity: 70%;
+    }
 `;
 
 export default CategoryFeed;
--- client/src/components/ConfirmationPage.js
+++ client/src/components/ConfirmationPage.js
@@ -13,29 +13,87 @@ const returnToProducts = () => {
 return (
         <>
         <Wrapper>
-        <p>thank you for shopping with us!</p>
+        <p className="gratitude">Thank you for shopping with us!</p>
         <StyledLogo src={logo}/>
-        <p>your order is confirmed</p>
+        <p>Your order is confirmed</p>
         <Button onClick={returnToProducts}>continue shopping?</Button>
         </Wrapper>
         </>
 );
 };
 
+
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+    justify-content: center;
+    height:80vh;
+    .gratitude{
+        font-size: 30px;
+    }
+    p{
+        font-size: 25px;
+    }
+    `;
+
 const StyledLogo = styled.img`
 height:100px;
-`
+`;
 
 const Button = styled.button`
-background:none;
-`
-const Wrapper = styled.div`
-display: flex;
-flex-direction:column;
-align-items: center;
-justify-content: center;
-height:80vh;
+    padding: 12px 40px;
+    margin: 15px;
+    font: 16px/24px Verdana, sans-serif;
+    text-decoration: none;
+    background: 
+        linear-gradient(90deg, 
+            #04f5ed, 
+            #5c27fe, 
+            #c165dd, 
+            #04f5ed);
+    background-size: 400%;
+    display: inline-block;
+    color: #fff;
+    border-radius: 8px;
+    position: relative;
+    border: none;
+    cursor: pointer;
+    animation: glowing 8s linear infinite;
+    text-transform: uppercase;
+    text-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+    letter-spacing: 1px;
+    &:after {
+        content: " ";
+        position: absolute;
+        border-radius: inherit;
+        left: -6px;
+        right: -6px;
+        bottom: -6px;
+        top: -6px;
+        background: inherit;
+        background-size: inherit;
+        z-index: -1;
+        opacity: 0;
+        transition: opacity 0.5s ease;
+        filter: blur(16px);
+        animation: glowing 7s linear infinite;
+        transform: translateZ(0);
+    }
+    &:hover {
+        &:after {
+            opacity: 1;
+        }   
+    }
+}
 
-`
+    @keyframes glowing {
+        100% {
+        background-position: -400%;
+        }
+
+`;
+
 
 export default ConfirmationPage;
