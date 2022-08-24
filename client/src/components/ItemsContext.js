9de7110b6c64fb1c19184f0d725f5399a705fa61
“Anna <annaovcharova84@gmail.com>
2022-08-24 10:16:13 -0400

    comments done

--- client/src/components/ItemsContext.js
+++ client/src/components/ItemsContext.js
@@ -2,7 +2,9 @@ import { createContext, useState, useEffect} from "react";
 
 export const ItemsContext = createContext(null);
 export const ItemsProvider = ({children}) => {
+    //variable to store selected category value
     const [selectedCaregory, setSelectedCategory] = useState("");
+    //variable to manipulate the quantity of items placed in the cart
     const [numCartItems, setNumCartItems] = useState(0);
     return(
         <ItemsContext.Provider value={{
